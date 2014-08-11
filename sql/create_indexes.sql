59127843d4f2177f018538aa3a5b17a805d69338
Sam Haycraft <phreakdawg@gmail.com>
2014-08-11 14:02:44 -0600

    fixes spelling error

--- sql/create_indexes.sql
+++ sql/create_indexes.sql
@@ -1,3 +1,3 @@
 CREATE INDEX idx_qq ON leases(qq);
 CREATE INDEX idx_leaseid ON leases(lease_id);
-CREATE INDEX idx_qq_gem ON plss.qq USING GIST(geom);
+CREATE INDEX idx_qq_geom ON plss.qq USING GIST(geom);
